4f1bb765f3acd73540ce369861f0653beb1e7b23
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-10-08 03:15:10 +0700

    test overflow-y contactsidebar

--- frontend/src/components/ContactSidebar.tsx
+++ frontend/src/components/ContactSidebar.tsx
@@ -5,7 +5,71 @@ function ContactSidebar() {
   return (
     <div className='bg-primary w-1/4 p-5 rounded-xl h-full overflow-y-auto overflow-x-auto fixed max-h-90vh top-24 right-2 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent'>
       <h3 className='text-white font-semibold text-lg mb-3'>Contact</h3>
-      <ul>
+      <ul className=''>
+        <li className='pb-5 flex justify-between items-center'>
+          <div className=''>
+            <div className='relative flex items-center'>
+              <div className='pr-2'>
+                <span className='notification-badge absolute top-[-4px] left-[-6px] w-4 h-4 bg-green-400 rounded-full flex items-center justify-center font-semibold'></span>
+                <img src='https://pixner.net/circlehub/main/assets/images/avatar-1.png' alt='' />
+              </div>
+              <div className='flex flex-col justify-between'>
+                <h3 className='font-semibold text-white text-lg'>Bao Linh</h3>
+              </div>
+            </div>
+          </div>
+          <div className='setting'>
+            <FontAwesomeIcon icon={faEllipsis} className='text-white' />
+          </div>
+        </li>
+        <li className='pb-5 flex justify-between items-center'>
+          <div className=''>
+            <div className='relative flex items-center'>
+              <div className='pr-2'>
+                <span className='notification-badge absolute top-[-4px] left-[-6px] w-4 h-4 bg-green-400 rounded-full flex items-center justify-center font-semibold'></span>
+                <img src='https://pixner.net/circlehub/main/assets/images/avatar-1.png' alt='' />
+              </div>
+              <div className='flex flex-col justify-between'>
+                <h3 className='font-semibold text-white text-lg'>Bao Linh</h3>
+              </div>
+            </div>
+          </div>
+          <div className='setting'>
+            <FontAwesomeIcon icon={faEllipsis} className='text-white' />
+          </div>
+        </li>
+        <li className='pb-5 flex justify-between items-center'>
+          <div className=''>
+            <div className='relative flex items-center'>
+              <div className='pr-2'>
+                <span className='notification-badge absolute top-[-4px] left-[-6px] w-4 h-4 bg-green-400 rounded-full flex items-center justify-center font-semibold'></span>
+                <img src='https://pixner.net/circlehub/main/assets/images/avatar-1.png' alt='' />
+              </div>
+              <div className='flex flex-col justify-between'>
+                <h3 className='font-semibold text-white text-lg'>Bao Linh</h3>
+              </div>
+            </div>
+          </div>
+          <div className='setting'>
+            <FontAwesomeIcon icon={faEllipsis} className='text-white' />
+          </div>
+        </li>
+        <li className='pb-5 flex justify-between items-center'>
+          <div className=''>
+            <div className='relative flex items-center'>
+              <div className='pr-2'>
+                <span className='notification-badge absolute top-[-4px] left-[-6px] w-4 h-4 bg-green-400 rounded-full flex items-center justify-center font-semibold'></span>
+                <img src='https://pixner.net/circlehub/main/assets/images/avatar-1.png' alt='' />
+              </div>
+              <div className='flex flex-col justify-between'>
+                <h3 className='font-semibold text-white text-lg'>Bao Linh</h3>
+              </div>
+            </div>
+          </div>
+          <div className='setting'>
+            <FontAwesomeIcon icon={faEllipsis} className='text-white' />
+          </div>
+        </li>
         <li className='pb-5 flex justify-between items-center'>
           <div className=''>
             <div className='relative flex items-center'>
