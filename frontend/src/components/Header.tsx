e480f55527e410206372e6d465fda5c48ed8dd6e
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-10-08 15:29:01 +0700

    Dropdown profile

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell, faFileLines, faFilm, faHome, faMessage, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faBell, faFileLines, faFilm, faGear, faHome, faMessage, faRightFromBracket, faUsers } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 function Header() {
+  const [showDropdown, setShowDropdown] = useState(false)
+  const handleDropdownToggle = () => {
+    setShowDropdown(!showDropdown)
+  }
   return (
     <header className=' p-3 border-none bg-primary fixed top-0 right-0 left-0 z-30'>
       <div className='flex items-center justify-between'>
@@ -47,9 +52,41 @@ function Header() {
             </span>
             <FontAwesomeIcon icon={faBell} className='text-2xl' />
           </span>
-          <div className='ml-2 relative'>
+          <div className='ml-2 relative cursor-pointer' onClick={handleDropdownToggle}>
             <span className='notification-badge absolute top-[-4px] left-[-6px] w-4 h-4 bg-green-400 rounded-full flex items-center justify-center font-semibold'></span>
             <img src='https://pixner.net/circlehub/main/assets/images/avatar-1.png' alt='avatar' />
+            {showDropdown && (
+              <div className='dropdown absolute bg-secondary w-56 right-0 mt-4 rounded-xl p-4'>
+                <div className='profile pb-5 '>
+                  <div className='relative flex'>
+                    <div className='pr-2'>
+                      <span className='notification-badge absolute top-[-4px] left-[-6px] w-4 h-4 bg-green-400 rounded-full flex items-center justify-center font-semibold'></span>
+                      <img src='https://pixner.net/circlehub/main/assets/images/avatar-1.png' alt='' />
+                    </div>
+                    <div className='flex flex-col justify-between'>
+                      <h3 className='font-semibold text-white text-lg'>Bao Linh</h3>
+                      <p className='font-medium text-white text-sm'>@baolinhdev</p>
+                    </div>
+                  </div>
+                </div>
+                <div className=' flex justify-center'>
+                  <button className='text-center bg-primary w-full py-1 rounded-sm text-white hover:text-primaryButton font-semibold'>
+                    View Profile
+                  </button>
+                </div>
+                <div></div>
+                <ul className='mt-3'>
+                  <li className='flex items-center py-2 border-gray-700 hover:text-primaryButton border-b'>
+                    <FontAwesomeIcon icon={faGear} className='mr-2 text-lg' />
+                    <p>Setting</p>
+                  </li>
+                  <li className='flex items-center py-2 border-gray-700 hover:text-primaryButton border-b'>
+                    <FontAwesomeIcon icon={faRightFromBracket} className='mr-2' />
+                    <p className='font-semibold'>Sign out</p>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
